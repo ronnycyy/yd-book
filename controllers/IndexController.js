7e3209b2579816d340878053e5ba664626040033
ronnycyy <m17722548276@163.com>
2020-11-22 21:57:47 +0800

    update index.html

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -4,8 +4,11 @@ class IndexController extends Controller {
     super();
   }
   async actionIndex(ctx){ 
-    // note: 要加await，等待渲染完毕，再给到body上面。由于加了awate，函数要声明成async
-    ctx.body = await ctx.render('index');
+    // throw new Error('custom error');
+    // note: 要加await，等待渲染完毕，再给到body上面。由于加了await，函数要声明成async
+    ctx.body = await ctx.render('index', {
+      message: '后端数据'
+    });
   }
 }
 module.exports = IndexController;
