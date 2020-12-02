d0bf93c07fe1b5e6d3ca387a172d9d3319229580
ronnycyy <m17722548276@163.com>
2020-12-02 22:31:02 +0800

    edit

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const Koa = require("koa"); // commonJS语法 require
-const co = require('co');
-const config = require('./config');
-const { historyApiFallback } = require('koa2-connect-history-api-fallback');
-const initController = require('./controllers');  // 初始化路由
-const errorHandler = require('./middlewares/errorHandler');
-const staticServer = require('koa-static');  //静态资源（网页）
-var render = require('koa-swig');
-const log4js = require("log4js");
+// const Koa = require("koa"); // commonJS语法 require
+// const co = require('co');
+// const config = require('./config');
+// const { historyApiFallback } = require('koa2-connect-history-api-fallback');
+// const initController = require('./controllers');  // 初始化路由
+// const errorHandler = require('./middlewares/errorHandler');
+// const staticServer = require('koa-static');  //静态资源（网页）
+// var render = require('koa-swig');
+// const log4js = require("log4js");
+
+// es6模块化
+import Koa from 'koa';
+import co from 'co';
+import config from './config';
+import { historyApiFallback } from 'koa2-connect-history-api-fallback';
+import initController from './controllers';
+import errorHandler from './middlewares/errorHandler';
+import staticServer from 'koa-static';
+import render from 'koa-swig';
+import log4js from 'log4js';
 
 const app = new Koa();
 
